5506aca6b8f41496fd58b3b4507ebd97e3c08b1a
test <test.tq.ta@gmail.com>
2022-10-19 21:55:41 +0300

    commited task 4

--- src/Lesson4/task4.java
+++ src/Lesson4/task4.java
@@ -2,6 +2,6 @@ package Lesson4;
 
 public class task4 {
     public static void main(String[] args) {
-        System.out.println("task 4, task 3 is not merged");
+        System.out.println("task 4, task 3 is already merged");
     }
 }
