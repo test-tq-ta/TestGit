4e33ef0d8f026e8ea849df8b97c936b4005b7024
test <test.tq.ta@gmail.com>
2022-10-19 20:42:27 +0300

    task 1 is done

---
+++ src/Lesson1/Homework1.java
@@ -0,0 +1,12 @@
+package Lesson1;
+
+public class Homework1 {
+
+    public static void main(String[] args) {
+        task1();
+    }
+
+    private static void task1(){
+        System.out.println("task1");
+    }
+}
